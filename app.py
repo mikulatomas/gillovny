93ce60403d505448b4aa0c2fa95f356a2cf09bf6
Tomáš Mikula <mail@tomasmikula.cz>
2018-12-10 19:18:27 +0100

    Clean project and add favicon

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ This file creates your application.
 
 import os
 import random
+from urllib.parse import parse_qs, urlparse
 
 import bleach
 
@@ -20,124 +21,59 @@ app = Flask(__name__)
 cache = Cache()
 
 cache_servers = os.environ.get('MEMCACHIER_SERVERS')
-if cache_servers == None:
+
+if cache_servers is None:
     # Fall back to simple in memory cache (development)
     cache.init_app(app, config={'CACHE_TYPE': 'simple'})
 else:
     cache_user = os.environ.get('MEMCACHIER_USERNAME') or ''
     cache_pass = os.environ.get('MEMCACHIER_PASSWORD') or ''
     cache.init_app(app,
-        config={'CACHE_TYPE': 'saslmemcached',
-                'CACHE_MEMCACHED_SERVERS': cache_servers.split(','),
-                'CACHE_MEMCACHED_USERNAME': cache_user,
-                'CACHE_MEMCACHED_PASSWORD': cache_pass,
-                'CACHE_OPTIONS': { 'behaviors': {
-                    # Faster IO
-                    'tcp_nodelay': True,
-                    # Keep connection alive
-                    'tcp_keepalive': True,
-                    # Timeout for set/get requests
-                    'connect_timeout': 2000, # ms
-                    'send_timeout': 750 * 1000, # us
-                    'receive_timeout': 750 * 1000, # us
-                    '_poll_timeout': 2000, # ms
-                    # Better failover
-                    'ketama': True,
-                    'remove_failed': 1,
-                    'retry_timeout': 2,
-                    'dead_timeout': 30}}})
+                   config={'CACHE_TYPE': 'saslmemcached',
+                           'CACHE_MEMCACHED_SERVERS': cache_servers.split(','),
+                           'CACHE_MEMCACHED_USERNAME': cache_user,
+                           'CACHE_MEMCACHED_PASSWORD': cache_pass,
+                           'CACHE_OPTIONS': {'behaviors': {
+                               # Faster IO
+                               'tcp_nodelay': True,
+                               # Keep connection alive
+                               'tcp_keepalive': True,
+                               # Timeout for set/get requests
+                               'connect_timeout': 2000, # ms
+                               'send_timeout': 750 * 1000, # us
+                               'receive_timeout': 750 * 1000, # us
+                               '_poll_timeout': 2000, # ms
+                               #Better failover
+                               'ketama': True,
+                               'remove_failed': 1,
+                               'retry_timeout': 2,
+                               'dead_timeout': 30}}})
 
 app.config['SECRET_KEY'] = str(os.environ.get('SECRET_KEY'))
 app.config['CONSUMER_KEY'] = str(os.environ.get('CONSUMER_KEY'))
 app.config['CONSUMER_SECRET'] = str(os.environ.get('CONSUMER_SECRET'))
 
-###
-# Routing for your application.
-###
-def get_id(id):
-    auth = OAuthHandler(app.config['CONSUMER_KEY'], app.config['CONSUMER_SECRET'])
-    api = tweepy.API(auth)
-
-    tweet = api.get_status(id, tweet_mode='extended')
-    text = tweet.full_text
-
-    images = []
-    videos = []
-
-    try:
-        if 'media' in tweet.extended_entities:
-            for media in tweet.extended_entities['media']:
-                if 'photo' in media['type']:
-                    images.append(media['media_url'])
-                elif ('animated_gif' in media['type']) or ('video' in media['type']):
-                    video = media['video_info']['variants'][0]['url']
-                    videos.append(video)
-    except AttributeError:
-        if 'media' in tweet.entities:
-            for media in tweet.entities['media']:
-                images.append(media['media_url'])
-
-    urls = []
-    instagram = []
-    youtube = []
-
-    from urllib.parse import urlparse, parse_qs
-
-    if 'urls' in tweet.entities:
-        for url in tweet.entities['urls']:
-            url = url['expanded_url']
-
-            parsed = urlparse(url)
-
-            if 'youtube' in parsed.netloc:
-                query = parse_qs(parsed.query)
-                video_id = query['v'][0]
-                youtube.append(video_id)
-            elif 'instagram' in parsed.netloc:
-                instagram_id = parsed.path.split('/')[2]
-                instagram.append(instagram_id)
-            else:
-                urls.append(url)
-
-    return jsonify(text=text,
-                   images=images,
-                   videos=videos,
-                   urls=urls,
-                   instagram=instagram,
-                   youtube=youtube)
-
-
-@app.route('/api/images')
-def api_images():
-    return get_id('1064146450010071040')
-
-@app.route('/api/youtube')
-def api_youtube():
-    return get_id('1062383537997971456')
-
-@app.route('/api/video')
-def api_video():
-    return get_id('1064577977898463234')
-
-@app.route('/api/instagram')
-def api_instagram():
-    return get_id('1064658936119218176')
-
-@app.route('/api/url')
-def api_url():
-    return get_id('1065044175211687936')
-
-
-
+# API view
 @app.route('/api/')
 def api():
+    # Load dataset
     tweets = get_dataset()
-    tweet = random.choice(tweets)
-    text = bleach.linkify(tweet.full_text)
 
+    # Pick random tweet
+    tweet = random.choice(tweets)
+
+    # Bleach will transform URLs to HTML links
+    text = bleach.linkify(tweet.full_text)
+    # Format username
+    user = '@{}'.format(tweet.user.screen_name)
+    retweet = tweet.retweet_count
+    favorite = tweet.favorite_count
     images = []
     videos = []
+    instagram = []
+    youtube = []
 
+    # Extract media from tweet
     try:
         if 'media' in tweet.extended_entities:
             for media in tweet.extended_entities['media']:
@@ -150,12 +86,6 @@ def api():
         if 'media' in tweet.entities:
             for media in tweet.entities['media']:
                 images.append(media['media_url'])
-
-    urls = []
-    instagram = []
-    youtube = []
-
-    from urllib.parse import urlparse, parse_qs
 
     if 'urls' in tweet.entities:
         for url in tweet.entities['urls']:
@@ -172,29 +102,25 @@ def api():
             elif 'instagram' in parsed.netloc:
                 instagram_id = parsed.path.split('/')[2]
                 instagram.append(instagram_id)
-            else:
-                urls.append(url)
 
-    user = '@{}'.format(tweet.user.screen_name)
-    retweet = tweet.retweet_count
-    favorite = tweet.favorite_count
-
+    # Return JSON data
     return jsonify(text=text,
                    images=images,
                    videos=videos,
-                   urls=urls,
                    instagram=instagram,
                    youtube=youtube,
                    user=user,
                    retweet=retweet,
                    favorite=favorite)
 
-
-
-
 @cache.cached(timeout=50, key_prefix='all_comments')
 def get_dataset():
-    auth = OAuthHandler(app.config['CONSUMER_KEY'], app.config['CONSUMER_SECRET'])
+    """
+    Cached dataset, Twitter data are requested only once per 50s
+    """
+    auth = OAuthHandler(app.config['CONSUMER_KEY'],
+                        app.config['CONSUMER_SECRET'])
+
     api = tweepy.API(auth)
 
     query = 'gillovny -filter:retweets'
@@ -212,7 +138,11 @@ def get_dataset():
                                result_type='recent').items():
 
         # Ignore retweets
-        if (not tweet.retweeted) and ('RT @' not in tweet.full_text) and (tweet.full_text[0] != '@'):
+        if ((not tweet.retweeted) and
+            ('RT @' not in tweet.full_text) and
+            (tweet.full_text[0] != '@')):
+
+            # Ignore quoted tweets
             if not tweet.is_quote_status:
                 dataset.append(tweet)
 
@@ -221,52 +151,13 @@ def get_dataset():
 
     return dataset
 
+
+# Simple homepage view
 @app.route('/')
 def home():
-    # tweets = get_dataset()
-    # # Select random tweet
-    # tweet = random.choice(tweets)
-    # text = tweet.full_text
-    # # text = tweet.extended_tweet['full_text']
-    # images = []
-    # videos = []
-    #
-    # try:
-    #     if 'media' in tweet.extended_entities:
-    #         for media in tweet.extended_entities['media']:
-    #             if 'photo' in media['type']:
-    #                 images.append(media['media_url'])
-    #             elif 'animated_gif' in media['type']:
-    #                 video = media['video_info']['variants'][0]['url']
-    #                 videos.append(video)
-    # except AttributeError:
-    #     if 'media' in tweet.entities:
-    #         for media in tweet.entities['media']:
-    #             images.append(media['media_url'])
-    #
-    # urls = []
-    # instagram = []
-    # youtube = []
-    #
-    # from urllib.parse import urlparse, parse_qs
-    #
-    # if 'urls' in tweet.entities:
-    #     for url in tweet.entities['urls']:
-    #         url = url['expanded_url']
-    #
-    #         parsed = urlparse(url)
-    #
-    #         if 'youtube' in parsed.netloc:
-    #             query = parse_qs(parsed.query)
-    #             video_id = query['v'][0]
-    #             youtube.append(video_id)
-    #         elif 'instagram' in parsed.netloc:
-    #             instagram_id = parsed.path.split('/')[2]
-    #             instagram.append(instagram_id)
-    #         else:
-    #             urls.append(url)
-    #
-    # return render_template('home.html', text=text, images=images, videos=videos, urls=urls, instagram=instagram, youtube=youtube, raw=tweet)
+    """
+    Homepage static view, data is provided via AJAX
+    """
     return render_template('home.html')
 
 
@@ -288,4 +179,4 @@ def page_not_found(error):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
